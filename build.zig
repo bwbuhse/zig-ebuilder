ad056acd61d1570023f682e19e2f6ef27d0668ba
Eric Joldasov <bratishkaerik@landless-city.net>
2024-12-18 08:59:54 +0500

    build.zig: update to new "create `Compile` from module" `std.Build` API
    Signed-off-by: Eric Joldasov <bratishkaerik@landless-city.net>

--- build.zig
+++ build.zig
@@ -19,30 +19,32 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "zig-ebuilder",
+    const main_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "mustache", .module = mod_mustache },
+            .{ .name = "Report", .module = mod_Report },
+        },
     });
-    exe.root_module.addImport("mustache", mod_mustache);
-    exe.root_module.addImport("Report", mod_Report);
 
+    const exe = b.addExecutable(.{
+        .name = "zig-ebuilder",
+        .root_module = main_mod,
+    });
     b.installDirectory(std.Build.Step.InstallDir.Options{
         .install_dir = .{ .custom = "share" },
         .install_subdir = "zig-ebuilder",
         .source_dir = b.path("share/"),
     });
-
     if (no_bin)
         b.getInstallStep().dependOn(&exe.step)
     else
         b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
@@ -50,15 +52,9 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe_unit_tests.root_module.addImport("mustache", mod_mustache);
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const unit_tests = b.addTest(.{ .root_module = main_mod });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
